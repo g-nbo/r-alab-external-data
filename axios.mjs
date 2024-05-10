6c55c40d7cce8d4fd3c6f46ac25a7379e8f69b66
Gabe <115848472+g-nbo@users.noreply.github.com>
2024-05-10 17:17:23 -0400

    Complete part 3

--- axios.mjs
+++ axios.mjs
@@ -59,22 +59,7 @@ axios("https://api.thecatapi.com/v1/images/search", {}, {
 
 // Part 1
 
-(async function initialLoad() {
-  const listOfBreeds = await axios(`https://api.thecatapi.com/v1/breeds`);
 
-  listOfBreeds.data.forEach(element => {
-
-    const option = document.createElement("option");
-
-    breedSelect.appendChild(option);
-
-    option.setAttribute("value", element.id)
-
-    option.textContent = element.name;
-
-  });
-  buildCarousel();
-})();
 
 
 
@@ -105,7 +90,47 @@ axios("https://api.thecatapi.com/v1/images/search", {}, {
  */
 
 // Part 2
-breedSelect.addEventListener("change", buildCarousel)
+
+
+
+
+
+
+
+
+
+/**
+ * 3. Fork your own sandbox, creating a new one named "JavaScript Axios Lab."
+ */
+/**
+ * 4. Change all of your fetch() functions to axios!
+ * - axios has already been imported for you within index.js.
+ * - If you've done everything correctly up to this point, this should be simple.
+ * - If it is not simple, take a moment to re-evaluate your original code.
+ * - Hint: Axios has the ability to set default headers. Use this to your advantage
+ *   by setting a default header with your API key so that you do not have to
+ *   send it manually with all of your requests! You can also set a default base URL!
+ */
+
+(async function initialLoad() {
+    const listOfBreeds = await axios(`https://api.thecatapi.com/v1/breeds`);
+  
+    listOfBreeds.data.forEach(element => {
+  
+      const option = document.createElement("option");
+  
+      breedSelect.appendChild(option);
+  
+      option.setAttribute("value", element.id)
+  
+      option.textContent = element.name;
+  
+    });
+    buildCarousel();
+  })();
+
+
+  breedSelect.addEventListener("change", buildCarousel)
 
 async function buildCarousel() {
   const catVal = breedSelect.value
@@ -152,30 +177,9 @@ async function buildCarousel() {
   
   }
   waiting();
+
 }
 
-
-
-
-
-
-
-
-/**
- * 3. Fork your own sandbox, creating a new one named "JavaScript Axios Lab."
- */
-
-// axios.mjs
-
-/**
- * 4. Change all of your fetch() functions to axios!
- * - axios has already been imported for you within index.js.
- * - If you've done everything correctly up to this point, this should be simple.
- * - If it is not simple, take a moment to re-evaluate your original code.
- * - Hint: Axios has the ability to set default headers. Use this to your advantage
- *   by setting a default header with your API key so that you do not have to
- *   send it manually with all of your requests! You can also set a default base URL!
- */
 /**
  * 5. Add axios interceptors to log the time between request and response to the console.
  * - Hint: you already have access to code that does this!
